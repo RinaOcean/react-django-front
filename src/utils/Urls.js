00205ec4732ae27c1a3e7ab90fd0a5bc418ac9a6
RinaOcean <70663656+RinaOcean@users.noreply.github.com>
2022-08-08 14:02:38 +0200

    adds for future download file

--- src/utils/Urls.js
+++ src/utils/Urls.js
@@ -1,3 +1,4 @@
 export const UPLOAD_URL = 'http://127.0.0.1:8000/';
 export const TOKEN_URL = "http://127.0.0.1:8000/login/token/";
 export const REG_URL = "http://127.0.0.1:8000/login/register/";
+export const BROWSE_URL = "http://127.0.0.1:8000/browse_sftp/";
