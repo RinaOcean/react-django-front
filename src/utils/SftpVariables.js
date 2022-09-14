07cb3c01b6a5703b0eec37de3877767b6ea65211
RinaOcean <70663656+RinaOcean@users.noreply.github.com>
2022-09-14 23:05:23 +0300

    Update SftpVariables.js

--- src/utils/SftpVariables.js
+++ src/utils/SftpVariables.js
@@ -1,7 +1,11 @@
 // for prod
-export const HOST_NAME = "mft.bcbssc.com";
-export const PORT = 6622;
-export const USER_NAME = "SVC_UCImedical";
+export const UPLOAD_HOST_NAME = "mft.bcbssc.com";
+export const UPLOAD_PORT = 6622;
+export const UPLOAD_USER_NAME = "SVC_UCImedical";
+
+// export const DOWNLOAD_HOST_NAME = "207.54.158.34";
+// export const DOWNLOAD_PORT = 22;
+// export const DOWNLOAD_USER_NAME = "uci1";
 
 // for dev
 // export const HOST_NAME = "192.168.1.106";
